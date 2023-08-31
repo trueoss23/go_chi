195091c608eb2b87a9b03c6045c63a828a569bf0
Igor At Home <trueoss1992@gmail.com>
2023-08-31 22:58:28 +0700

    Defenition routers and handlers

--- handlers/book_handlers.go
+++ handlers/book_handlers.go
@@ -1,77 +1,73 @@
-package routes
+package handlers
 
 import (
 	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/trueoss23/go_chi/models"
 )
 
-func SetupRoutes(db models.BooksRepo) *chi.Mux {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+type Handler struct {
+	DB models.BooksRepo
+}
 
-	r.Get("/books", func(w http.ResponseWriter, r *http.Request) {
-		books, err := db.GetAll()
+func (h *Handler) GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	books, err := h.DB.GetAll()
 
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(books)
-	})
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
 
-	r.Post("/book", func(w http.ResponseWriter, r *http.Request) {
-		var book models.BookModel
-		err := json.NewDecoder(r.Body).Decode(&book)
+func (h *Handler) CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book models.BookModel
+	err := json.NewDecoder(r.Body).Decode(&book)
 
-		if err != nil {
-			http.Error(w, "Invalid request payload", http.StatusBadRequest)
-			return
-		}
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
 
-		fmt.Println(book)
-		err = db.Insert(book)
+	fmt.Println(book)
+	err = h.DB.Insert(book)
 
-		if err != nil {
-			http.Error(w, "Failed to insert data", http.StatusInternalServerError)
-			return
-		}
+	if err != nil {
+		http.Error(w, "Failed to insert data", http.StatusInternalServerError)
+		return
+	}
 
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("Data inserted successfully"))
-	})
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Data inserted successfully"))
+}
 
-	r.Delete("/book/{id}", func(w http.ResponseWriter, r *http.Request) {
-		var bookID string = string(chi.URLParam(r, "id"))
-		err := db.Delete(bookID)
+func (h *Handler) DeleteBook(w http.ResponseWriter, r *http.Request) {
+	bookID := chi.URLParam(r, "id")
+	err := h.DB.Delete(bookID)
 
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "Book deleted successfully")
-	})
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Book deleted successfully")
+}
 
-	r.Get("/book/{id}", func(w http.ResponseWriter, r *http.Request) {
-		var bookID string = string(chi.URLParam(r, "id"))
-		book, err := db.Get(bookID)
+func (h *Handler) GetBook(w http.ResponseWriter, r *http.Request) {
+	bookID := chi.URLParam(r, "id")
+	book, err := h.DB.Get(bookID)
 
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(book)
-	})
-
-	return r
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
 }
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 	cfg "github.com/trueoss23/go_chi/config"
 	"github.com/trueoss23/go_chi/db"
 	"github.com/trueoss23/go_chi/handlers"
@@ -12,7 +14,15 @@ import (
 
 func main() {
 	db := &db.MySQLDatabase{}
-	r := handlers.SetupRoutes(db)
+	// r := handlers.SetupRoutes(db)
+	h := &handlers.Handler{DB: db}
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/books", h.GetAllBooks) // Используем методы хэндлера вместо анонимных функций
+	r.Post("/book", h.CreateBook)
+	r.Delete("/book/{id}", h.DeleteBook)
+	r.Get("/book/{id}", h.GetBook)
 
 	fmt.Println("Server listening on port " + cfg.Cfg.AppPort)
 	log.Fatal(http.ListenAndServe(":"+cfg.Cfg.AppPort, r))
